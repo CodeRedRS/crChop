3c781c7bdd4f116becd0d008f74972d9f1fd3290
CodeRedRS <zeketiki@gmail.com>
2015-09-30 10:26:39 -0700

    Update crChop.java

--- crChop/crChop.java
+++ crChop/crChop.java
@@ -28,7 +28,7 @@ import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
 @Script.Manifest(
         name = "crChop",
         description = "AIO Woodcutter",
-        properties = "topic=1283889, client=4"
+        properties = "topic=1283889;client=4;"
 )
 public class crChop extends PollingScript<ClientContext> implements PaintListener, MessageListener {
 
